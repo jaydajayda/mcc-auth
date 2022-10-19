47db55fb600bdfb2976b5ceef6e6689e06b1372f
Sam Brinker <sam.a.brinker@gmail.com>
2022-10-19 16:36:43 -0400

    auth service implemented, TokenAPI formatted to work w front end client and data service

---
+++ src/main/java/com/metro/auth/api/TokenAPI.java
@@ -0,0 +1,134 @@
+package com.metro.auth.api;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.metro.auth.domain.Customer;
+import com.metro.auth.domain.CustomerFactory;
+import com.metro.auth.domain.Token;
+import com.metro.auth.util.JWTHelper;
+
+@RestController
+@RequestMapping("/token")
+public class TokenAPI {
+	
+	
+	String dataApiHost = "localhost:8080";
+	
+	//private static Key key = AuthFilter.key;	
+	public static Token appUserToken;
+	
+	@GetMapping
+	public String getAll() {
+		return "jwt-fake-token-asdfasdfasfa".toString();
+	}
+	
+	@PostMapping
+	//public ResponseEntity<?> createTokenForCustomer(@RequestBody Customer customer, HttpRequest request, UriComponentsBuilder uri) {
+	public ResponseEntity<?> createTokenForCustomer(@RequestBody Customer customer) {
+		
+		String username = customer.getName();
+		String password = customer.getPassword();
+		
+		if (username != null && username.length() > 0 && password != null && password.length() > 0 && checkPassword(username, password)) {
+			
+			Token token = createToken(username);
+			ResponseEntity<?> response = ResponseEntity.ok(token);
+			return response;			
+		}
+		// bad request
+		return (ResponseEntity<?>) ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+		
+	}
+	
+	private boolean checkPassword(String username, String password) {
+		// special case for application user
+		if(username.equals("ApiClientApp") && password.equals("secret")) {
+			return true;
+		}
+		// make call to customer service 
+		Customer cust = getCustomerByNameFromCustomerAPI(username);
+		
+		// compare name and password
+		if(cust != null && cust.getName().equals(username) && cust.getPassword().equals(password)) {
+			return true;				
+		}		
+		return false;
+		
+		
+		
+		
+		
+
+	}
+	
+	public static Token getAppUserToken() {
+		if(appUserToken == null || appUserToken.getToken() == null || appUserToken.getToken().length() == 0) {
+			appUserToken = createToken("ApiClientApp");
+		}
+		return appUserToken;
+	}
+	
+    private static Token createToken(String username) {
+    	String scopes = "com.metro.data.api";
+    	// special case for application user
+    	if( username.equalsIgnoreCase("ApiClientApp")) {
+    		scopes = "com.metro.auth.api";
+    	}
+    	String token_string = JWTHelper.createToken(scopes);
+    	return new Token(token_string);
+    }
+    
+    
+	private Customer getCustomerByNameFromCustomerAPI(String username) {
+		try {
+			String apiHost = System.getenv("API_HOST");
+			if(apiHost == null) {
+				apiHost = this.dataApiHost;
+			}
+			
+			URL url = new URL("http://" + apiHost + "/api/customers/byname/" + username);
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setRequestMethod("GET");
+			conn.setRequestProperty("Accept", "application/json");
+			Token token = getAppUserToken();
+			conn.setRequestProperty("authorization", "Bearer " + token.getToken());
+
+			if (conn.getResponseCode() != 200) {
+				return null;
+			} else {
+				BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
+				String output = "";
+				String out = "";
+				while ((out = br.readLine()) != null) {
+					output += out;
+				}
+				conn.disconnect();
+				System.out.println(CustomerFactory.getCustomer(output));
+				return CustomerFactory.getCustomer(output);
+			}
+
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+			return null;
+
+		} catch (java.io.IOException e) {
+			e.printStackTrace();
+			return null;
+		}
+
+	}  	
+
+}    
+
